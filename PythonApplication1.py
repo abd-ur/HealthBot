700e2f304c381f976c10fd9dd4c9fbdbb6a94b62
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-03-31 15:18:58 +0530

    Update PythonApplication1.py

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -2,10 +2,6 @@ import pandas as p
 import numpy as n
 import streamlit as st
 st.title('MyApp')
-import pandas as p
-import numpy as n
-import streamlit as st
-st.title('MyApp')
 if st.button('Say hello'):
     st.write('Get lost')
 else:
