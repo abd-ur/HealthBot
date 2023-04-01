5f01cf46aec4e45fcb70bb7591e83f397baaba20
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-01 10:06:46 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,12 @@
+USERS = {
+    'alice': 'password123',
+    'bob': 'pass123',
+    'charlie': 'password'}
+st.write('Please log in')
+us=st.text_input('Username')
+pas=st.text_input('Password',type='password')
+if st.button('Log in'):
+    if us in USERS and pas==USERS[us]:
+        st.success('Logged in as {}'.format(us))
+    else:
+        st.error('Invalid credentials')
