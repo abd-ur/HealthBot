cb535f562a5610db8d97ebcdaf4d3196bd8f7fa9
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-01 09:51:02 +0530

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,15 @@
+USERS = {
+    'alice': 'password123',
+    'bob': 'pass123',
+    'charlie': 'password'}
+def login():
+    st.write('Please log in')
+    username = st.text_input('Username')
+    password = st.text_input('Password', type='password')
+    if st.button('Log in'):
+        if username in USERS and password == USERS[username]:
+            st.success('Logged in as {}'.format(username))
+        else:
+            st.error('Invalid username or password')
+if __name__ == '__main__':
+    login()
