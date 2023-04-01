279845fa45f61902e954051fd314eed7359d15a7
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-01 10:32:07 +0530

    Update login.py

--- login.py
+++ login.py
@@ -9,6 +9,12 @@ us=st.text_input('Username')
 pas=st.text_input('Password',type='password')
 if st.button('Log in'):
     if us in USERS and pas==USERS[us]:
-        subprocess.Popen(['streamlit', 'run', 'https://mangekkyo-testapp-pythonapplication1-ob04e2.streamlit.app'])
+        st.title('MyApp')
+        if st.button('Say hello'):
+            st.write('Get lost')
+        else:
+            st.write('Win it all')
+        title=st.text_input('Movie title', 'Life of Brian')
+        st.write('The current movie title is', title)
     else:
-        st.error('Invalid credentials')
+         st.error('Invalid credentials')
