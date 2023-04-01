1291ba40ecc9e7297bef57400cec853dfeafda87
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-01 10:14:36 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,13 +1,3 @@
 import streamlit as st
-USERS = {
-    'alice': 'password123',
-    'bob': 'pass123',
-    'charlie': 'password'}
-st.write('Please log in')
-us=st.text_input('Username')
-pas=st.text_input('Password',type='password')
-if st.button('Log in'):
-    if us in USERS and pas==USERS[us]:
-        st.success('Logged in as {}'.format(us))
-    else:
-        st.error('Invalid credentials')
+if st.button():
+    subprocess.Popen(['streamlit', 'run', 'https://mangekkyo-testapp-pythonapplication1-ob04e2.streamlit.app/'])
