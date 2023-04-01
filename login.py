e8865ad10cc514531b476ebcf2f979aa73c92a94
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-01 10:16:33 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,4 +1,5 @@
 import streamlit as st
+st.write('pass')
 if st.button():
     st.write('ok')
 #    subprocess.Popen(['streamlit', 'run', 'https://mangekkyo-testapp-pythonapplication1-ob04e2.streamlit.app/'])
