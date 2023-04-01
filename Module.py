81cd0746fdd97af432ec788bce47eb811e20b533
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-01 10:00:31 +0530

    Update Module.py

--- Module.py
+++ Module.py
@@ -2,13 +2,11 @@ USERS = {
     'alice': 'password123',
     'bob': 'pass123',
     'charlie': 'password'}
-def login():
-    st.write('Please log in')
-    username = st.text_input('Username')
-    password = st.text_input('Password', type='password')
-    if st.button('Log in'):
-        if username in USERS and password == USERS[username]:
-            st.success('Logged in as {}'.format(username))
-        else:
-            st.error('Invalid username or password')
-login()
+st.write('Please log in')
+us=st.text_input('Username')
+pass=st.text_input('Password',type='password')
+if st.button('Log in'):
+    if us in USERSand pass==USERS[us]:
+        st.success('Logged in as {}'.format(us))
+    else:
+        st.error('Invalid credentials')
