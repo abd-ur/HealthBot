d421f8b52d46534c85a010049b081dffc8da5790
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-01 10:01:32 +0530

    Update Module.py

--- Module.py
+++ Module.py
@@ -4,7 +4,7 @@ USERS = {
     'charlie': 'password'}
 st.write('Please log in')
 us=st.text_input('Username')
-pass=st.text_input('Password',type='password')
+pas=st.text_input('Password',type='password')
 if st.button('Log in'):
     if us in USERSand pass==USERS[us]:
         st.success('Logged in as {}'.format(us))
