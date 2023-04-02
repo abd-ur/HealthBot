866bf20fb90e2686a64ef0832ac9e95dee3977d9
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 15:09:02 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,11 +1,13 @@
 import streamlit as st
 import random
+ph=st.empty()
 st.title("Hi I am LIFE, your personal health care assistant. You can start chatting.")
 rules={
     "hi": ["Hello", "Hi"],
     'checkup':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.'],
     'diabetes':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.'],
     'sugar':['Would you like to go for a quick diagnosis?'],
+    'yes':['Alright then we will proceed'],
     'no':['Ok we will take it on someother time.'],
     "bye": ["Goodbye, Take care", "Bye!"],
     "default": ["Sorry, I could'nt understand."]}
@@ -16,5 +18,19 @@ def match(inp):
 inp=st.text_input("You:","")
 if st.button("Send"):
     res=match(inp)
-    st.text_area("Bot:",res)
-    st.text_input("You:","",value='')
+    st.text_area("LIFE:",res)
+    if res=='Alright then we will proceed':
+        with ph.container():
+          age=st.number_input("Enter your Age",min_value=0,max_value=150)
+          gen=st.radio("Select your Gender",["Male","Female"])
+          bmi=st.number_input("Enter your Body Mass Index",min_value=9,max_value=50)
+          ins=st.number_input("Enter your recently diagnosed Insulin Level",max_value=300)
+          glu=st.number_input("Enter your recently diagnosed Glucose Level",min_value=60,max_value=200)
+          bp=st.number_input("Enter your Upper Blood Pressure",min_value=50,max_value=220)
+          if st.button('Submit'):
+              st.success('Your records are saved.')
+              time.sleep(2)
+              ph.empty()
+              st.write('Age :',age,'\nGender :',gen,'\nBody mass index :',bmi,'\nInsulin :',ins,'\nGlucose :',glu,'\nBlood Pressure :',bp)
+   
+                    
