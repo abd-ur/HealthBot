a8119a59ead1febe04f3c4bbe3711d8495aeddc7
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 14:44:58 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import streamlit as st
 import random
+st.title("Hi I am LIFE, your personal health care assistant. You can start chatting.")
+def match(inp):
+    res=rules.get(inp.lower(),rules["default"])
+    return random.choice(res)
 rules={
     "hi": ["Hello", "Hi"],
     "bye": ["Goodbye, Take care", "Bye!"],
     "default": ["Sorry, I could'nt understand."]
-def match(inp):
-    res=rules.get(inp.lower(),rules["default"])
-    return random.choice(res)
-st.title("Hi I am LIFE, your personal health care assistant. You can start chatting.")
+
 inp=st.text_input("You:","")
 if st.button("Send"):
     res=match(inp)
