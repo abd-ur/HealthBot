0d288b83fbe169baa6260490ed22152cae13b885
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 11:22:51 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,15 @@
 import streamlit as st
-st.write('working')
-st.SessionState.loggin_in='True'
+
+# Define the options for the selectbox
+options = ["Home", "Page 1", "Page 2"]
+
+# Create a sidebar with the selectbox
+selection = st.sidebar.selectbox("Go to", options)
+
+# Show the appropriate content based on the selected option
+if selection == "Home":
+    st.write("This is the home page.")
+elif selection == "Page 1":
+    st.write("This is page 1.")
+elif selection == "Page 2":
+    st.write("This is page 2.")
