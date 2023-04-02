db16487aa3011d6edbb5838c5d7a25e187df8e39
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 11:31:56 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,15 +1,3 @@
 import streamlit as st
-    
-st.title('Login')
-USERS = {
-        'alice': 'password123',
-        'bob': 'pass123',
-        'charlie': 'password'}
-st.write('Please log in')
-us=st.text_input('Username')
-pas=st.text_input('Password',type='password')
-if st.button('Log in'):
-    if us in USERS and pas==USERS[us]:
-        st.write('Welcome : ',us)
-    else:
-        st.error('Invalid credentials')
+import SessionState
+st.write('working')
