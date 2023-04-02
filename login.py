9ba93e13d5a68465b70734f69362c69dc34ab1cc
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 14:44:09 +0530

    Update login.py

--- login.py
+++ login.py
@@ -5,11 +5,11 @@ rules={
     "hi": ["Hello", "Hi"],
     "bye": ["Goodbye, Take care", "Bye!"],
     "default": ["Sorry, I could'nt understand."]
-def rule(inp):
+def match(inp):
     res=rules.get(inp.lower(),rules["default"])
     return random.choice(res)
 st.title("Hi I am LIFE, your personal health care assistant. You can start chatting.")
 inp=st.text_input("You:","")
 if st.button("Send"):
-    res=rule(inp)
+    res=match(inp)
     st.text_area("Bot:",res)
