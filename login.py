b7f5d436f6f856a85b2f1555ca6ca6f8ebb39ba3
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 11:48:15 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,3 +1,34 @@
 import streamlit as st
-import SessionState
-st.write('working')
+def login():   
+  while(1):
+    st.title('Login')
+    USERS = {
+            'alice': 'password123',
+            'bob': 'pass123',
+            'charlie': 'password'}
+    st.write('Please log in')
+    us=st.text_input('Username')
+    pas=st.text_input('Password',type='password')
+    if st.button('Log in'):
+        if us in USERS and pas==USERS[us]:
+            return us
+            break
+        else:
+            st.error('Invalid credentials, try again.')
+
+
+# Define the options for the selectbox
+options = ["Home", "Login", "Alice",'Bob']
+
+# Create a sidebar with the selectbox
+selection = st.sidebar.selectbox("Go to",'Home','Login')
+
+# Show the appropriate content based on the selected option
+if selection == "Home":
+    st.write("This is the home page.")
+elif selection == "Login":
+    selection=login()
+if selection=='Alice':
+  st.write('welcome alice')
+elif selection=='Bob':
+  st.write('welcome bob')
