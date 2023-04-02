0376163bc938f9e761f62944af0094b7750012dd
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 14:55:16 +0530

    Update login.py

--- login.py
+++ login.py
@@ -3,11 +3,16 @@ import random
 st.title("Hi I am LIFE, your personal health care assistant. You can start chatting.")
 rules={
     "hi": ["Hello", "Hi"],
+    'checkup':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.']
+    'diabetes':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.']
+    'sugar':['Would you like to go for a quick diagnosis?']
+    'no':['Ok we will take it on someother time.]
     "bye": ["Goodbye, Take care", "Bye!"],
     "default": ["Sorry, I could'nt understand."]}
 def match(inp):
-    res=rules.get(inp.lower(),rules["default"])
-    return random.choice(res)
+    for key in rules:
+        if key in inp:
+          return random.choice(rules[key])
 inp=st.text_input("You:","")
 if st.button("Send"):
     res=match(inp)
