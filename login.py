4ddd964de00e18344aa58d6108d82c11b0f6bb5b
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 12:05:35 +0530

    Update login.py

--- login.py
+++ login.py
@@ -3,9 +3,9 @@ import streamlit as st
 def main():
     st.write("Hello, world!")
     if st.button("Clear"):
-        empty_container = st.empty()
-        empty_container.empty()
-
-main()
+        # Remove all widgets from the app
+        # This also removes the sidebar and header
+        # but keeps the footer
+        st.experimental_set_query_params()
 
 
