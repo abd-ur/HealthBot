bc27c14afe58843a8b4420acc479d0601f06aee5
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 13:22:39 +0530

    Update login.py

--- login.py
+++ login.py
@@ -13,7 +13,7 @@ def login():
         pas=st.text_input('Password',type='password')
         if st.button('Log in'):
             if us in USERS and pas==USERS[us]:
-                return us  
+                st.write('yes')  
             else:
                 st.error('Invalid credentials, try again.')
 def checkup(x):
@@ -27,8 +27,9 @@ def checkup(x):
   if st.button('Submit'):
     st.success('Your records are saved.')
     x[alice]=age,gen
-usr=login() 
-ph.empty()
-st.title('Welcome', usr,'lets check') 
-checkup(records)
-print(records[usr])
+login()
+#usr=login() 
+#ph.empty()
+#st.title('Welcome', usr,'lets check') 
+#checkup(records)
+#print(records[usr])
