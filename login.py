bd4330c83c767644c9141420725796d7e0befac7
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 12:18:41 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,16 +1,28 @@
 import streamlit as st
-    
-st.title('Login')
-USERS = {
-        'alice': 'password123',
-        'bob': 'pass123',
-        'charlie': 'password'}
-st.write('Please log in')
-us=st.text_input('Username')
-pas=st.text_input('Password',type='password')
-if st.button('Log in'):
-    if us in USERS and pas==USERS[us]:
+
+def login():
+    # Show the login form
+    username = st.text_input("Username")
+    password = st.text_input("Password", type="password")
+    if st.button("Login"):
+        # Verify the login credentials
+        if username == "admin" and password == "123":
+            # Clear the login form and show the main screen
+            username = ""
+            password = ""
+            st.success("Logged in!")
+            main()
+        else:
+            st.error("Invalid username or password")
+
+def main():
+    # Show the main screen
+    st.write("Hello, world!")
+    if st.button("Logout"):
+        # Clear the main screen and show the login form
         st.experimental_set_query_params()
-        st.write('Welcome : ',us)
-    else:
-        st.error('Invalid credentials')
+        login()
+
+if __name__ == '__main__':
+    # Start with the login form
+    login()
