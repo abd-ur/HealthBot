0983ba4de6cc26b48e2d0ce0f666c5905b55b403
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 14:56:44 +0530

    Update login.py

--- login.py
+++ login.py
@@ -3,10 +3,10 @@ import random
 st.title("Hi I am LIFE, your personal health care assistant. You can start chatting.")
 rules={
     "hi": ["Hello", "Hi"],
-    'checkup':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.']
-    'diabetes':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.']
-    'sugar':['Would you like to go for a quick diagnosis?']
-    'no':['Ok we will take it on someother time.']
+    'checkup':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.'],
+    'diabetes':['Shall we start your diabetes diagnosis?','Ok then, we will begin your diagnosis then.'],
+    'sugar':['Would you like to go for a quick diagnosis?'],
+    'no':['Ok we will take it on someother time.'],
     "bye": ["Goodbye, Take care", "Bye!"],
     "default": ["Sorry, I could'nt understand."]}
 def match(inp):
