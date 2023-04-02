75c206c26c700b04ddae595a8c385ed3203565e9
mangekkyo <115999605+mangekkyo@users.noreply.github.com>
2023-04-02 14:00:22 +0530

    Update login.py

--- login.py
+++ login.py
@@ -1,2 +1,24 @@
 import streamlit as st
-from streamlit import SessionState
+ph=st.empty()
+records={'alice':None,'bob':None}
+def login():
+  us=''
+  with ph.container():
+          st.title('Login')
+          USERS = {
+                  'alice': 'password123',
+                  'bob': 'pass123',
+                  'charlie': 'password'}
+          st.write('Please log in')
+          us=st.text_input('Username')
+          pas=st.text_input('Password',type='password')
+          if st.button('Log in'):
+              if us in USERS and pas==USERS[us]:
+                  ph.empty()
+                  return us
+              else:
+                  st.error('Invalid credentials, try again.')
+if st.button('Sign in'):
+  usr=login()
+if usr!='':
+  st.success('Welcome :,'usr)
